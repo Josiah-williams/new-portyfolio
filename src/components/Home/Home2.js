b5ff533aac19c92bc44ca8736af291c5c2345fdc
josiah-williams <josiahdamiwilliams@gmail.com>
2022-01-08 13:29:12 +0100

    added pdf and button

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/avataaars.svg";
 import "../../style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Tilt from "react-parallax-tilt";
+import Button from "react-bootstrap/Button";
+import pdf from "../../Assets/josiah-williams cv.pdf";
 
 function Home2() {
   return (
@@ -15,25 +17,7 @@ function Home2() {
               Let me <span className="purple"> introduce </span> myself
             </h1>
             <p className="home-about-body">
-              A highly motivated person, dynamic, persevering and rigorous.
-              <br />
-              <br /> I'm{" "}
-              <b className="purple">Web & Mobile Engineering Student </b>
-              at ENSIAS, Rabat. My Graduation will be in 2022.
-              <br />
-              <br />
-              I’m also ex-chair and mentor of
-              <b className="purple"> IEEE ENSIAS Student Branch</b>, Co-Founder
-              and Program Manager of
-              <b className="purple"> Morocco IoT & AI Challenge</b>, a capacity
-              building and pre-incubation program for students and startups that
-              have innovative ideas in the areas of Internet of Things (IoT),
-              Artificial Intelligence (AI) and related fields.
-              <br />
-              <br />
-              I'm also a<b className="purple"> content creator </b> on Instagram
-              where I mix between my graphic design skills and sharing my coding
-              knowledge.
+            I am an energetic professional who has a natural approach to presentation design, navigation design and information design and at the same time a great appetite and readiness to take responsibility, and release my professional and personal competencies so that together we can lift tomorrow's challenges. As a Web developer, I have gained familiarity with front-end and back-end core concepts. As a person, I am analytical and passionate about showing what I can and enjoy challenging myself personally and professionally. During my education and courses, I have gained a great knowledge of Photoshop. Understands the importance of being able to document my work, which I perceive as one of my strengths, just as I find it easy to familiarize myself with new applications and systems. I enjoy being in charge of tasks and working independently just like in teams.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
@@ -48,10 +32,11 @@ function Home2() {
             <p>
               Feel free to <span className="purple">connect </span>with me
             </p>
+            <h2>Josiahdamiwilliams@gmail.com</h2>
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/choubari"
+                  href="https://github.com/Josiah-williams"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -61,7 +46,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/choubari_"
+                  href="https://twitter.com/Josiah51039920"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -71,7 +56,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/kawtar-choubari-2226b0150/"
+                  href="https://www.linkedin.com/in/josiahwilliams"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -90,6 +75,10 @@ function Home2() {
                 </a>
               </li>
             </ul>
+            <p>Checkout my resume</p>
+            <Button variant="primary" href={pdf} target="_blank">
+            <i className="fas fa-download">&nbsp;</i>Download Resume
+          </Button>
           </Col>
         </Row>
       </Container>
